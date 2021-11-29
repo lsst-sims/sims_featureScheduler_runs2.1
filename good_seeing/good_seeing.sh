3850861a88592457a0b95c2da5f6d73ecf187068
Peter Yoachim <yoachim@uw.edu>
2021-11-29 10:24:09 -0800

    trying out good seeing weights

---
+++ good_seeing/good_seeing.sh
@@ -0,0 +1,7 @@
+python good_seeing.py --gsw 0
+python good_seeing.py --gsw 1
+python good_seeing.py --gsw 3
+python good_seeing.py --gsw 6
+python good_seeing.py --gsw 10
+python good_seeing.py --gsw 20
+python good_seeing.py --gsw 50
