3233962e464b875e83094f6a7459d2792ae7434a
Peter Yoachim <yoachim@uw.edu>
2021-12-14 17:05:54 -0800

    good seeing in u

---
+++ good_seeing/good_seeing_u.sh
@@ -0,0 +1,7 @@
+python good_seeing_u.py --gsw 0
+python good_seeing_u.py --gsw 1
+python good_seeing_u.py --gsw 3
+python good_seeing_u.py --gsw 6
+python good_seeing_u.py --gsw 10
+python good_seeing_u.py --gsw 20
+python good_seeing_u.py --gsw 50
