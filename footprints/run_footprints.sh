165bfb133d0f186c1d10d836035828fda76b7650
Peter Yoachim <yoachim@uw.edu>
2022-01-26 14:48:55 -0800

    run script

---
+++ footprints/run_footprints.sh
@@ -0,0 +1,14 @@
+python pencil.py --field_selection 1
+python pencil.py --field_selection 2
+python plane_priority.py --pencilbeams --gal_priority 0
+python plane_priority.py --pencilbeams --gal_priority 0.3
+python plane_priority.py --pencilbeams --gal_priority 0.6
+python plane_priority.py --pencilbeams --gal_priority 0.9
+python plane_priority.py --pencilbeams --gal_priority 1.2
+python plane_priority.py --pencilbeams --gal_priority 1.5
+python plane_priority.py --gal_priority 0
+python plane_priority.py --gal_priority 0.3
+python plane_priority.py --gal_priority 0.6
+python plane_priority.py --gal_priority 0.9
+python plane_priority.py --gal_priority 1.2
+python plane_priority.py --gal_priority 1.5
