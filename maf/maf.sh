a295a19c175a0947b30eb671e79d7ba31a2b483c
Peter Yoachim <yoachim@uw.edu>
2022-02-18 09:58:53 -0800

    ugh

--- maf/maf.sh
+++ maf/maf.sh
@@ -1,9 +1,9 @@
 ddf_dir --db baseline_v2.1_10yrs.db  
-ddf_dir --db ddf_double_slf0.20_v2.1_10yrs.db  
+ddf_dir --db ddf_season_length_slf0.20_v2.1_10yrs.db
 ddf_dir --db ddf_euclid_moved_slf0.20_v2.1_10yrs.db
 scimaf_dir --long_micro --db baseline_v2.1_10yrs.db  
-scimaf_dir --long_micro --db ddf_double_slf0.20_v2.1_10yrs.db  
+scimaf_dir --long_micro --db ddf_season_length_slf0.20_v2.1_10yrs.db
 scimaf_dir --long_micro --db ddf_euclid_moved_slf0.20_v2.1_10yrs.db
 glance_dir --db baseline_v2.1_10yrs.db  
-glance_dir --db ddf_double_slf0.20_v2.1_10yrs.db  
+glance_dir --db ddf_season_length_slf0.20_v2.1_10yrs.db
 glance_dir --db ddf_euclid_moved_slf0.20_v2.1_10yrs.db
