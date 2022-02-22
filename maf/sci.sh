700e1f6f1340ebeb72cac03b906896c7f3c4adad
Peter Yoachim <yoachim@uw.edu>
2022-02-21 16:33:32 -0800

    just do the science

---
+++ maf/sci.sh
@@ -0,0 +1,3 @@
+scimaf_dir --long_micro --db baseline_v2.1_10yrs.db  
+scimaf_dir --long_micro --db ddf_season_length_slf0.20_v2.1_10yrs.db
+scimaf_dir --long_micro --db ddf_euclid_moved_slf0.20_v2.1_10yrs.db
