f5d9ee83331cf16df45d7a3c082afa14d7a80033
Peter Yoachim <yoachim@uw.edu>
2022-04-07 15:43:23 -0700

    sh file

---
+++ technical/supress_repeat/no_repeat.sh
@@ -0,0 +1,6 @@
+python no_repeat.py --repeat_weight 1
+python no_repeat.py --repeat_weight 2
+python no_repeat.py --repeat_weight 5
+python no_repeat.py --repeat_weight 10
+python no_repeat.py --repeat_weight 20
+python no_repeat.py --repeat_weight 100
