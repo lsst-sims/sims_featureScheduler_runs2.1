3f00c0d602515a40ad99b4f66ad5a970689f6daa
Peter Yoachim <yoachim@uw.edu>
2022-04-27 17:11:32 -0700

    twi maf

---
+++ technical/twi_neo/maf.sh
@@ -0,0 +1,14 @@
+scimaf_dir --db twi_neo_nightpattern1v2.1_10yrs.db
+scimaf_dir --db twi_neo_nightpattern2v2.1_10yrs.db
+scimaf_dir --db twi_neo_nightpattern3v2.1_10yrs.db
+scimaf_dir --db twi_neo_nightpattern4v2.1_10yrs.db
+scimaf_dir --db twi_neo_nightpattern5v2.1_10yrs.db
+scimaf_dir --db twi_neo_nightpattern6v2.1_10yrs.db
+scimaf_dir --db twi_neo_nightpattern7v2.1_10yrs.db
+glance_dir --db twi_neo_nightpattern1v2.1_10yrs.db
+glance_dir --db twi_neo_nightpattern2v2.1_10yrs.db
+glance_dir --db twi_neo_nightpattern3v2.1_10yrs.db
+glance_dir --db twi_neo_nightpattern4v2.1_10yrs.db
+glance_dir --db twi_neo_nightpattern5v2.1_10yrs.db
+glance_dir --db twi_neo_nightpattern6v2.1_10yrs.db
+glance_dir --db twi_neo_nightpattern7v2.1_10yrs.db
