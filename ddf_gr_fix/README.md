d476bab74e351e0542cbf001e33d3bdafc60027a
Peter Yoachim <yoachim@uw.edu>
2022-06-01 16:57:27 -0700

    gr fix

---
+++ ddf_gr_fix/README.md
@@ -0,0 +1,1 @@
+most of the DDFs were run with the desired g and r sequences flipped. Fixing that here
---
+++ ddf_gr_fix/run_gr.sh
@@ -0,0 +1,6 @@
+python ddf_gr_fix.py --ddf_season_frac 0.1
+python ddf_gr_fix.py --ddf_season_frac 0.15
+python ddf_gr_fix.py --ddf_season_frac 0.2
+python ddf_gr_fix.py --ddf_season_frac 0.25
+python ddf_gr_fix.py --ddf_season_frac 0.30
+python ddf_gr_fix.py --ddf_season_frac 0.35
